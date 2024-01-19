8f5742b52d28038de8e82522f31e56384defa56e
demo0033 <gabin.demorgny@etudiant.univ-reims.fr>
2024-01-19 12:21:31 +0100

    Configure typeOrm annotation for place entity

--- src/Place.ts
+++ src/Place.ts
@@ -1,6 +1,16 @@
-export class Place {
+import {BaseEntity, Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+
+@Entity()
+export class Place extends BaseEntity{
+    @PrimaryGeneratedColumn()
     id!: number;
+
+    @Column()
     name!: string;
+
+    @Column()
     latitude!: number;
+
+    @Column()
     longitude!: number;
 }
